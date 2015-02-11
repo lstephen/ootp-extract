021fcc44458e07e30fc160a0a4cb1d039342ce41
lestephen <lestephen@groupon.com>
2015-02-11 14:52:38 -0800

    Add sanity test

---
+++ src/test/java/com/ljs/ootp/extract/TestSanity.java
@@ -0,0 +1,15 @@
+package com.ljs.ootp.extract;
+
+import org.fest.assertions.Assertions;
+
+import org.junit.Test;
+
+public class TestSanity {
+
+  @Test
+  public void sane() {
+    Assertions.assertThat(true).isTrue();
+  }
+
+}
+
